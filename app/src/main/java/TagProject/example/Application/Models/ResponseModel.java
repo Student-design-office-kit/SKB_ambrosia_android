877708ddd42a60883d87a99ef0afa43facd8f85d
bratusev07 <bratusevd@mail.ru>
2022-07-27 13:19:24 +0300

    Почистил файлы моделей

--- app/src/main/java/TagProject/example/Application/Models/ResponseModel.java
+++ app/src/main/java/TagProject/example/Application/Models/ResponseModel.java
@@ -4,14 +4,6 @@ import com.google.gson.annotations.SerializedName;
 
 /** Модель ответа от сервера*/
 public class ResponseModel {
-
-    @SerializedName("md5")
-    private MD5 md5;
-
-    public MD5 getMd5() {
-        return md5;
-    }
-
     @SerializedName("msg")
     private String msg;
 
